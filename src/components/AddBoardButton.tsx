ffee23fb992a50362d7e26ef355d7566651da44b
Palanbyul Choi <chlvkfksquf@gmail.com>
2025-02-20 02:22:42 +0900

    Modify AddBoardButton style

--- src/components/AddBoardButton.tsx
+++ src/components/AddBoardButton.tsx
@@ -5,7 +5,7 @@ export default function AddBoardButton() {
 
   return (
     <button
-      className="h-11 text-gray-700 bg-gray-300 rounded-md p-2.5 min-w-72 flex justify-between items-center"
+      className="h-11 text-gray-700 text-sm bg-gray-300 rounded-md p-2.5 min-w-72 flex justify-between items-center"
       onClick={addBoard}
     >
       새 보드 추가
