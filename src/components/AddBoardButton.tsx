81600b23326153dd856ad0e5743cf17e2745323a
Palanbyul Choi <chlvkfksquf@gmail.com>
2025-02-19 21:15:17 +0900

    Add AddBoardButton

---
+++ src/components/AddBoardButton.tsx
@@ -0,0 +1,14 @@
+import { useBoardStore } from '@/hooks/useBoardStore';
+
+export default function AddBoardButton() {
+  const { addBoard } = useBoardStore();
+
+  return (
+    <button
+      className="h-11 text-gray-700 bg-gray-300 rounded-md p-2.5 min-w-72 flex justify-between items-center"
+      onClick={addBoard}
+    >
+      새 보드 추가
+    </button>
+  );
+}
